186ae70a06a12d7949040d02da9ea97d1cd27c3e
Samuel Murari <sammurari17@gamil.com>
2022-02-14 02:45:16 +0530

    Junit

---
+++ Assignments/Junit&Mockito/BankAccountTest.java
@@ -0,0 +1,26 @@
+package io.Junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BankAccountTest {
+	BankAccount b;
+	
+	@BeforeEach
+	void object() {
+		b=new BankAccount();
+		
+	}
+
+	@Test
+	void test1() {assertThrows(InsufficientFundsExpcetion.class,()->b.withdraw(21000));}
+	
+	@Test
+	void test2() {assertThrows(InsufficientFundsExpcetion.class,()->b.withdraw(3000));}
+	
+	@Test
+	void test3() {assertThrows(InsufficientFundsExpcetion.class,()->b.withdraw(20000));}
+
+}
---
+++ Assignments/Junit&Mockito/Lifecycle.java
@@ -0,0 +1,17 @@
+package io.Junit;
+
+public class Lifecycle {
+	public int add(int a,int b) {
+		return a+b;
+	}
+public int sub(int a,int b) {
+	return a-b;
+}
+public int mul(int a,int b) {
+	return a*b;
+}
+public int div(int a,int b) {
+	return a/b;
+}
+
+}
